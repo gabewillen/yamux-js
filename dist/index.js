a394a39535c2c2d612cc2a81fa3b0369244038eb
gabewillen <gabewillen@gmail.com>
2024-10-22 17:54:29 -0500

    export stream types

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Session = exports.Stream = exports.Server = exports.Client = void 0;
+exports.Duplex = exports.Transform = exports.Writable = exports.Readable = exports.Session = exports.Stream = exports.Server = exports.Client = void 0;
 const client_1 = require("./client");
 Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return client_1.Client; } });
 const server_1 = require("./server");
@@ -9,3 +9,8 @@ const stream_1 = require("./stream");
 Object.defineProperty(exports, "Stream", { enumerable: true, get: function () { return stream_1.Stream; } });
 const session_1 = require("./session");
 Object.defineProperty(exports, "Session", { enumerable: true, get: function () { return session_1.Session; } });
+const readable_stream_1 = require("readable-stream");
+Object.defineProperty(exports, "Readable", { enumerable: true, get: function () { return readable_stream_1.Readable; } });
+Object.defineProperty(exports, "Writable", { enumerable: true, get: function () { return readable_stream_1.Writable; } });
+Object.defineProperty(exports, "Transform", { enumerable: true, get: function () { return readable_stream_1.Transform; } });
+Object.defineProperty(exports, "Duplex", { enumerable: true, get: function () { return readable_stream_1.Duplex; } });
